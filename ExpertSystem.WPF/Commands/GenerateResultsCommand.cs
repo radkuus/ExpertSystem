d85ae4605f2101769393498d39273b1bf22a65f1
Gipsonek123 <134952200+Gipsonek123@users.noreply.github.com>
2025-09-11 15:52:22 +0200

    Modify GenerateResultCommand

--- ExpertSystem.WPF/Commands/GenerateResultsCommand.cs
+++ ExpertSystem.WPF/Commands/GenerateResultsCommand.cs
@@ -16,6 +16,9 @@ using System.Net.Http;
 using System.IO;
 using ExpertSystem.WPF.State.Navigators;
 using ExpertSystem.WPF.Helpers.IfThen;
+using ExpertSystem.WPF.Helpers.Sample;
+using System.Printing.IndexedProperties;
+
 
 namespace ExpertSystem.WPF.Commands
 {
@@ -52,11 +55,11 @@ namespace ExpertSystem.WPF.Commands
         }
         public bool CanExecute(object? parameter)
         {
-            return true;
+            return _viewModel.AreDetailsChecked;
         }
-
         public async void Execute(object? parameter)
         {
+
             try
             {
                 // check server status
@@ -72,6 +75,30 @@ namespace ExpertSystem.WPF.Commands
                 var training_size = _viewModel.SelectedTrainingSetPercentage;
                 var data = new List<List<string>>();
 
+                var user_samples_raw = _viewModel.UserSample;
+                var user_samples = new List<List<string>>();
+                var samples = new Dictionary<string, string>();
+
+                if (user_samples_raw == null)
+                {
+                    user_samples = null;
+                }
+                else
+                {
+                    foreach (var row in user_samples_raw.UserSamples)
+                    {
+                        foreach (var entry in row)
+                        {
+                            // lista dla API
+                            user_samples.Add(new List<string> { entry.ColumnName, entry.Value });
+                        }
+                    }
+                }
+
+                var results = new List<ModelAnalysisResult>();
+                var hyperparameters = new Dictionary<string, string>();
+                var decisionRules = new List<DecisionRule>();
+
                 List<string> selectedColumns = new List<string>(analysis_columns);
                 selectedColumns.Add(target_column);
 
@@ -86,9 +113,8 @@ namespace ExpertSystem.WPF.Commands
                 }
 
 
-                var results = new List<ModelAnalysisResult>();
-                var hyperparameters = new Dictionary<string, string>();
-                MessageBox.Show("Generating results");
+
+                MessageBox.Show("Generating results"); // tymczasowe
 
                 // KNN
                 if (_viewModel.IsKnnChecked)
@@ -102,13 +128,18 @@ namespace ExpertSystem.WPF.Commands
                         target_column,
                         training_size = float.Parse(training_size),
                         neighbors = int.Parse(neighbors),
-                        distance_metric
+                        distance_metric,
+                        user_samples
                     };
 
                     var response = await _apiService.PostAsync<ModelAnalysisResult>("/knn", request);
                     response.ModelName = "KNN";
                     results.Add(response);
                     hyperparameters["KNN"] = JsonSerializer.Serialize(new { neighbors });
+
+                    if (user_samples != null)
+                        samples["KNN"] = JsonSerializer.Serialize(user_samples);
+
                 }
 
                 // Bayes
@@ -119,14 +150,37 @@ namespace ExpertSystem.WPF.Commands
                         data,
                         analysis_columns,
                         target_column,
-                        training_size = float.Parse(training_size)
+                        training_size = float.Parse(training_size),
+                        user_samples
 
                     };
                     var response = await _apiService.PostAsync<ModelAnalysisResult>("/bayes", request);
                     response.ModelName = "Bayes";
                     results.Add(response);
                     hyperparameters["Bayes"] = null;
+                    if (user_samples != null)
+                        samples["Bayes"] = JsonSerializer.Serialize(user_samples);
                 }
+
+                // LR model
+                if (_viewModel.IsLogisticRegressionChecked)
+                {
+                    var request = new
+                    {
+                        data,
+                        analysis_columns,
+                        target_column,
+                        training_size = float.Parse(training_size),
+                        user_samples
+                    };
+                    var response = await _apiService.PostAsync<ModelAnalysisResult>("/lr", request);
+                    response.ModelName = "LogisticRegression";
+                    results.Add(response);
+                    hyperparameters["LogisticRegression"] = null;
+                    if (user_samples != null)
+                        samples["LogisticRegression"] = JsonSerializer.Serialize(user_samples);
+                }
+
 
                 // NeuralNetwork
                 if (_viewModel.IsNeuralNetworkChecked)
@@ -142,36 +196,105 @@ namespace ExpertSystem.WPF.Commands
                         neurons,
                         layers,
                         target_column,
-                        training_size = float.Parse(training_size)
+                        training_size = float.Parse(training_size),
+                        user_samples
 
                     };
                     var response = await _apiService.PostAsync<ModelAnalysisResult>("/NeuralNetwork", request);
                     response.ModelName = "NeuralNetwork";
                     results.Add(response);
-                    hyperparameters["NeuralNetwork"] = null;
+                    hyperparameters["NeuralNetwork"] = JsonSerializer.Serialize(new { layers, neurons });
+                    if (user_samples != null)
+                        samples["NeuralNetwork"] = JsonSerializer.Serialize(user_samples);
                 }
 
-                if (_viewModel.IsLogisticRegressionChecked)
+                if (_viewModel.IsIfThenChecked)
                 {
+                    var ifthen = new List<List<string>>();
+                    decisionRules = new List<DecisionRule>(); // reset
+
+                    // dla kazdej reguly
+                    foreach (var row in _viewModel.IfThenConditions)
+                    {
+                        // szukanie pierwszego warunku, który ma "then"- zwraca tylko jak znajdzie (mozna potem zablokowac przycisk
+                        // Generate jesli brakuje then)
+                        var thenClass = row.Conditions.FirstOrDefault(c => c.Type == "then")?.SelectedClass ?? "";
+
+                        // znalezienie indeksu ostatniego warunku, który nie ma "then"
+                        int lastNonThenIndex = -1;
+                        for (int i = 0; i < row.Conditions.Count; i++)
+                        {
+                            if (row.Conditions[i].Type != "then")
+                                lastNonThenIndex = i;
+                        }
+
+                        for (int i = 0; i < row.Conditions.Count; i++)
+                        {
+                            var col = row.Conditions[i];
+
+                            // dodanie do ifthen dla API
+                            ifthen.Add(new List<string>
+                            {
+                                col.Type ?? "",
+                                col.SelectedColumn ?? "",
+                                col.SelectedOperator ?? "",
+                                col.SelectedValue?.ToString() ?? "",
+                                col.SelectedClass ?? ""
+                            });
+
+                            // tworzenie DecisionRule tylko dla warunków bez then
+                            if (col.Type != "then")
+                            {
+                                bool isLastNonThen = (i == lastNonThenIndex);
+
+                                var rule = new DecisionRule
+                                {
+                                    Column = col.SelectedColumn ?? "",
+                                    Operator = col.SelectedOperator switch
+                                    {
+                                        ">" => Operator.GreaterThan,
+                                        "<" => Operator.LessThan,
+                                        "=" => Operator.Equal,
+                                    },
+                                    Threshold = col.SelectedValue != null ? Convert.ToDouble(col.SelectedValue) : 0.0,
+                                    LogicOperator = isLastNonThen ? LogicOperator.Or : LogicOperator.And, // OR w bazie jesli ostatni warunek konczy się
+                                                                                                          // then, w innym przypadku AND
+                                    Result = thenClass
+                                };
+
+                                decisionRules.Add(rule);
+                            }
+                        }
+                    }
+
                     var request = new
                     {
                         data,
                         analysis_columns,
                         target_column,
-                        training_size = float.Parse(training_size)
+                        user_samples,
+                        ifthen
                     };
-                    var response = await _apiService.PostAsync<ModelAnalysisResult>("/lr", request);
-                    response.ModelName = "LogisticRegression";
+
+                    var response = await _apiService.PostAsync<ModelAnalysisResult>("/ifthen", request);
+                    response.ModelName = "Own";
                     results.Add(response);
-                    hyperparameters["LogisticReggresion"] = null;
+                    hyperparameters["Own"] = null;
+
+                    if (user_samples != null)
+                        samples["Own"] = JsonSerializer.Serialize(user_samples);
                 }
 
 
+
+                // DB SAVE 
                 await _experimentService.CreateExperimentWithResults(
                     userId: dataset.UserId,
                     datasetId: dataset.Id,
                     analysisResults: results,
-                    hyperparameters: hyperparameters
+                    hyperparameters: hyperparameters,
+                    samples: samples,
+                    decisionRules: decisionRules
                 );
 
                 var resultsVm = _resultsFactory();
