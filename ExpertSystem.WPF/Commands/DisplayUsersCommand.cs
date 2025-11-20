b39e1db0aa29ac6108295964c4ed2580df9dc2bc
radkus <szwed502@gmail.com>
2025-11-20 16:38:49 +0100

    admins can now update their own profile data in the admin panel

--- ExpertSystem.WPF/Commands/DisplayUsersCommand.cs
+++ ExpertSystem.WPF/Commands/DisplayUsersCommand.cs
@@ -19,17 +19,23 @@ namespace ExpertSystem.WPF.Commands
     {
         private readonly AdminViewModel _adminViewModel;
         private readonly IUserService _userService;
+        private readonly IAuthenticator _authenticator;
 
-        public DisplayUsersCommand(AdminViewModel adminViewModel, IUserService userService)
+        public DisplayUsersCommand(AdminViewModel adminViewModel, IUserService userService, IAuthenticator authenticator)
         {
             _adminViewModel = adminViewModel;
             _userService = userService;
+            _authenticator = authenticator;
         }
 
         public override async Task ExecuteAsync(object? parameter)
         {
             var users = await _userService.GetAll();
+            var adminId = _authenticator.CurrentUser.Id;
+            var admin = users.FirstOrDefault(u => u.Id == adminId);
+
             _adminViewModel.Users.Clear();
+            _adminViewModel.Users.Add(admin);
             foreach (var user in users)
             {
                 if (!user.IsAdmin)
